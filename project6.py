8be06e3644c9d0ef0d89cfb696cea560c6859b4b
Wesley Dashner <wdashner11@gmail.com>
2021-03-16 18:22:14 -0600

    test all combinations and average from each month

--- project6.py
+++ project6.py
@@ -37,15 +37,53 @@ january_points = january_points.groupby('Segment_id', as_index=False)['Observed'
 sns.scatterplot(data=january_points, x='Segment_id', y='Observed')
 plt.show()
 # %%
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from statistics import mean 
+import itertools
+
+def test():
+    # train_set, test_set = train_test_split(points)
+    feature_cols = ['Evapotranspiration', 'Precipitation', 'Irrigation_pumping']
+    for i in range(1, len(feature_cols) + 1):
+        for subset in itertools.combinations(feature_cols, i):
+            scores = []
+            for month in range(1, 13):
+                points = dataset[dataset['Month'] == month]
+                points = points.groupby('Segment_id', as_index=False).mean()
+                subset = list(subset)
+                X = points[subset]
+                y = points['Observed']
+                lm = LinearRegression()
+                lm.fit(X, y)
+                scores.append(lm.score(X, y))
+            print(subset)
+            print(mean(scores))
+            print()
+            # X_new = pd.DataFrame({column: [50, 55, 60]})
+            # predictions = lm.predict(test_set[subset].values)
+            # iterator = [row for row in train_set.iterrows()]
+            # differences = []
+            # for i in range(len(predictions)):
+            #     differences.append(iterator[i][1]['Observed'] - predictions[i])
+            # print(month)
+            # print(mean(differences))
+            # print()
+
+
+
 def plot_observed_vs_segment(month):
     points = dataset[dataset['Month'] == month]
     points = points.groupby('Segment_id', as_index=False).mean()
     for column in points.columns:
-        if column not in ['Observed', 'Month']:
-            sns.scatterplot(data=points, x=column, y='Observed')
-            plt.show()
+        if column not in ['Observed', 'Month', 'Date', 'x', 'y', 'Segment_id']:
+            # sns.scatterplot(data=points, x=column, y='Observed')
+            # plt.show()
+            test(points, column)
 
-for i in range(1, 12 + 1):
-    plot_observed_vs_segment(i)
+# for i in range(1, 12 + 1):
+#     test(i)
+
+test()
 
 # %%
